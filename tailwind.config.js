ae310df139da2b6bae8c4b968fae7b00215fb210
PaingSC <paingsoechit.job@gmail.com>
2023-03-18 20:43:08 +0630

    Creating Heder navi and Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./portfolio/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main": "#FFF",
+        "subcolor": "#BCBCBC",
+        "background": "#0F1624",
+      },
+      fontFamily: {
+        'body': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        'base': ['18px', '1.75'],
+        'base-sm': ['16px', '1.75'],
+        'nav': ['20px', '1.75'],
+        'nav-sm': ['18px', '1.75'],
+        'lg': ['24px', '1.75'],
+        'lg-sm': ['18px', '1.75'],
+        'ttl': ['42px', '1.75'],
+        'ttl-sm': ['28px', '1.75'],
+      },
+      screens: {
+        'tabv': {'max': '1000px'},
+        'sm': {'max': '768px'},
+      }  
+    },
+
   },
   plugins: [],
 }
